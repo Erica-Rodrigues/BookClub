c4979d529d8a6080541b12f660f5306ee5f172f1
Erica <erica.soso@hotmail.com>
2025-04-10 14:12:49 +0200

    accueil structure

---
+++ src/accueil.php
@@ -0,0 +1,27 @@
+<?php
+
+    require "./header.php";
+
+?>
+
+
+<div class="container__accueil">
+    <div class="accueil__img">
+        <img src="" alt="">
+    </div>
+    <div class="accueil__content">
+        <h1 class="accueil-content__title"></h1>
+        <p class="accueil-content__description"></p>
+    </div>
+</div>
+
+
+
+
+
+
+<?php
+
+    require "./footer.php"
+
+?>
