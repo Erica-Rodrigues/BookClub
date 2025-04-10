ec4ca6b093aaede2b482914e11c3d2db9dbd7778
Erica <erica.soso@hotmail.com>
2025-04-10 14:53:46 +0200

    ajout de contenue + image et stylisation du contenue

--- src/accueil.php
+++ src/accueil.php
@@ -7,10 +7,11 @@
 
 <div class="container__accueil">
     <div class="accueil__img">
-        <img src="" alt="">
+        <img src="https://i.pinimg.com/736x/9d/8b/b0/9d8bb0df052f1cedc767c5c2d3d45ad6.jpg" alt="livres" class="img__accueil">
     </div>
     <div class="accueil__content">
-        <h1 class="accueil-content__title"></h1>
+        <h1 class="accueil-content__title">The book club</h1>
+        <h2 class="accueil-content__subtitle">You didn't know you needed</h2>
         <p class="accueil-content__description"></p>
     </div>
 </div>
