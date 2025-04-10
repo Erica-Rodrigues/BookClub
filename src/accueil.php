c752205cb8930683bf86cac83c554e36f4886a6d
Erica <erica.soso@hotmail.com>
2025-04-10 15:27:19 +0200

    ajout d'un paragraphe + modification des margin et

--- src/accueil.php
+++ src/accueil.php
@@ -12,7 +12,16 @@
     <div class="accueil__content">
         <h1 class="accueil-content__title">The book club</h1>
         <h2 class="accueil-content__subtitle">You didn't know you needed</h2>
-        <p class="accueil-content__description"></p>
+        <p class="accueil-content__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Reiciendis quia numquam provident quod id modi, placeat tempora exercitationem sit animi.
+            <br><br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Reiciendis quia numquam provident quod id modi, placeat tempora exercitationem sit animi.
+            <br><br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Reiciendis quia numquam provident quod id modi, placeat tempora exercitationem sit animi.
+        </p>
     </div>
 </div>
 
