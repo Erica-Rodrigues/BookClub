448833afda07bfd0853f829ad3af3e0fb91f475d
Erica <erica.soso@hotmail.com>
2025-04-13 17:31:44 +0200

    création footer

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,11 @@
-
+<div class="container">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center  pb-3 mb-3" style="border-bottom: 2px solid rgb(173, 193, 120);">
+        <li class="nav-item"><a href="./accueil.php" class="nav-link px-2 text-body-secondary">Home</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">&copy; 2025 BookClub, Inc</p>
+    </footer>
+</div>
 <script src="./assets/js/form.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 </body>
